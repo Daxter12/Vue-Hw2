7154edf63a252889eb80ef1c766557fb9e2e05f4
Daxter <daxternw@gmail.com>
2022-03-15 15:00:01 +0500

    VueHw3

---
+++ src/components/MyButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click="onClick">Clicked {{ counter }} times!</button>
+</template>
+
+<script>
+  export default {
+    name: "MyButton",
+    data() {
+      return {
+        counter: 0,
+      };
+    },
+    methods: {
+      onClick() {
+        this.counter++;
+      },
+      onMouseOver() {},
+    },
+    beforeCreate() {},
+    created() {},
+    beforeMount() {},
+    mounted() {
+      const btn = this.$el.querySelector("button");
+      if (!btn) return;
+      btn.addEventListener("mouseover", this.onMouseOver);
+    },
+    beforeUpdate() {},
+    updated() {},
+    beforeDestroy() {
+      const btn = this.$el.querySelector("button");
+      if (!btn) return;
+      btn.removeEventListener("mouseover", this.onMouseOver);
+    },
+    destroyed() {},
+  };
+</script>
