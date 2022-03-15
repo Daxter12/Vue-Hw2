47a74ecfeed02b899060385652aeba5d1bec6389
Daxter <daxternw@gmail.com>
2022-03-15 15:00:01 +0500

    VueHw3

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <!-- <HelloWorld msg="привет мир" /> -->
-    <Calc />
+    <header>
+      <div class="title">My personal costs</div>
+    </header>
+
+    <main>
+      <add-payment-form @addPayment="add" />
+      <payments-display :items="paymentsList" />
+    </main>
   </div>
 </template>
 
 <script>
-  // import HelloWorld from "./components/HelloWorld.vue";
-  import Calc from "./components/Calc.vue";
+  import AddPaymentForm from "./components/AddPaymentForm.vue";
+  import PaymentsDisplay from "./components/PaymentsDisplay.vue";
 
   export default {
     name: "App",
-    components: {
-      Calc,
+    components: { PaymentsDisplay, AddPaymentForm },
+    data() {
+      return {
+        show: false,
+        paymentsList: [],
+      };
+    },
+    methods: {
+      add(data) {
+        this.paymentsList = [...this.paymentsList, data];
+      },
+      fetchData() {
+        return [
+          {
+            id: "1",
+            date: "28.03.2020",
+            category: "Food",
+            value: 169,
+          },
+          {
+            id: "2",
+            date: "24.03.2020",
+            category: "Transport",
+            value: 360,
+          },
+          {
+            id: "3",
+            date: "24.03.2020",
+            category: "Food",
+            value: 532,
+          },
+        ];
+      },
+    },
+    created() {
+      this.paymentsList = this.fetchData();
     },
   };
 </script>
